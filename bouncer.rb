aa6f64437659ccf9deaa78b2718fe078fa95d3e0
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-08-25 14:41:43 -0500

    normal mode

--- bouncer.rb
+++ bouncer.rb
@@ -1,14 +1,24 @@
+@inf = 1.0 / 0.0
+
 def check_age
   #ask age
   puts "Hello, how old are you?"
   age = gets.chomp.to_i
 
-  case permissions
-    when age < 18
-      puts "Sorry, you have no permissions"
-    when age >= 18
+  case age
+    when 0..18
+      puts "Sorry, you have no permissions. "
+    when 18..21
       puts "You can vote AND smoke! Go enjoy."
+    when 21..25
+      puts "You can vote, smoke, and drink a beer."
+    when 25..@inf
+      puts "You can vote, smoke tobacco, drink alcohol and rent a car. PLease don't drink and drive."
+    end
 end
+
+check_age
+
 
 #print what i can do at that age or older
   #if younger than 18 nothing
