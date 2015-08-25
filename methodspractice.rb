30b8b9df89c3d2c9e5f834fa2bd3370206d8b74c
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-08-25 11:55:49 -0500

    Squareulator simplemethod

---
+++ methodspractice.rb
@@ -0,0 +1,31 @@
+def double (x)
+  x * 2
+end
+
+def square (x)
+  x * x
+end
+
+
+
+
+system('clear')
+puts "Welcome to the Squareulator."
+puts "Would you like to (D)ouble or (s)quare a number?"
+print "(D os S)"
+response = gets.chomp.upcase
+if response == "D"
+  print "What number do you want to double?"
+  response_number = gets.chomp.to_i
+  puts "The Double of  #{response_number} is #{double(response_number)}"
+  elsif response == "S"
+  print "What number do you want to square?"
+  response_number = gets.chomp.to_i
+  puts "The Square of #{response_number} is #{square(response_number)}"
+elsif response == "Q"
+  puts "Goodbye!"
+  exit
+else
+  puts "What?"
+end
+
