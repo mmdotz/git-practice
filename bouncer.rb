f93a568b05a6f64e9dae4efce0a75136582b2d7a
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-08-25 16:11:51 -0500

    Revert "messing up nightmare mode part2"
    This reverts commit 156f6c772aef25aad3eedb4d4e04f825c3a7770e.

--- bouncer.rb
+++ bouncer.rb
@@ -1,53 +1,43 @@
 @inf = 1.0 / 0.0
 
 def check_age
-  puts "Do you live in the UK or US?"
-  country = gets.chomp.upcase
   puts "Hello, how old are you? To quit, type 0 (zero)."
   age = gets.chomp.to_i
+  puts "Do you live in the UK or US?"
+  country = gets.chomp.upcase
 
+if country == "US"
+  case age
+    when 1..18
+      puts "Sorry, no permissions. "
+    when 18..21
+      puts "You can vote AND smoke! Go enjoy."
+    when 21..25
+      puts "You can vote, smoke, and drink a beer."
+    when 25..@inf
+      puts "You can vote, smoke tobacco, drink alcohol and rent a car. Please don't drink and drive."
+    when 0
+      puts "Thank you. Goodbye."
+      exit
+    end
 
-  if country == "UK" || country == "US"
+  else
     case age
-      when 1..18
-        puts "Sorry, no permissions. "
+    when 18..@inf
+      puts "In the UK, at age 18 you may enjoy a pint."
+    when 16 || 17
+      puts "Are your parents with you? T/F?"
+      parents = gets.chomp.upcase
+      if parents == "T"
+        puts "You may order a drink."
+      else
+        puts "You have a few years before you can enjoy one with the rents."
       end
-
-  if country == "US" || country == "UK"
-    case age
-      when 18..21
-        puts "You can vote AND smoke! Go enjoy."
-      when 21..25
-        puts "You can vote, smoke, and drink a beer."
-      when 25..@inf
-        puts "You can vote, smoke tobacco, drink alcohol and rent a car. Please don't drink and drive."
-      when 0
-        puts "Thank you. Goodbye."
-        exit
-      end
-
-    elsif country == "UK"
-      case age
-      when 18..@inf
-        puts "In the UK, age 18 and older, you may enjoy a pint."
-      when 16 || 17
-        puts "Are your parents with you? T/F?"
-        parents = gets.chomp.upcase
-        if parents == "T"
-          puts "You may order a drink."
-        else
-          puts "You have a little while before you can enjoy one with the 'rents."
-        end
-      end
-
-    else
-      puts "PLease enter only UK or US."
     end
 end
-
+end
 
 loop do
 check_age
-exit
 end
 
