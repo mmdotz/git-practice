bb60e3190570d858dd8e087907576093da4bb7a2
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-08-25 11:59:57 -0500

    deleted test file and added methods practice

--- methodspractice.rb
+++ methodspractice.rb
@@ -8,24 +8,24 @@ end
 
 
 
-
 system('clear')
 puts "Welcome to the Squareulator."
-puts "Would you like to (D)ouble or (s)quare a number?"
-print "(D os S)"
-response = gets.chomp.upcase
-if response == "D"
-  print "What number do you want to double?"
-  response_number = gets.chomp.to_i
-  puts "The Double of  #{response_number} is #{double(response_number)}"
-  elsif response == "S"
-  print "What number do you want to square?"
-  response_number = gets.chomp.to_i
-  puts "The Square of #{response_number} is #{square(response_number)}"
-elsif response == "Q"
-  puts "Goodbye!"
-  exit
-else
-  puts "What?"
+loop do
+  puts "Would you like to (D)ouble or (s)quare a number?"
+  print "(D os S)"
+  response = gets.chomp.upcase
+  if response == "D"
+    print "What number do you want to double?"
+    response_number = gets.chomp.to_i
+    puts "The Double of  #{response_number} is #{double(response_number)}"
+    elsif response == "S"
+    print "What number do you want to square?"
+    response_number = gets.chomp.to_i
+    puts "The Square of #{response_number} is #{square(response_number)}"
+  elsif response == "Q"
+    puts "Goodbye!"
+    exit
+  else
+    puts "What?"
+  end
 end
-
