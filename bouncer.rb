5b407202e8f4972a1c0dfe3d2db81158d757911e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-08-25 15:08:21 -0500

    nightmare

--- bouncer.rb
+++ bouncer.rb
@@ -1,15 +1,15 @@
 @inf = 1.0 / 0.0
 
-
-
 def check_age
-  #ask age
   puts "Hello, how old are you? To quit, type 0 (zero)."
   age = gets.chomp.to_i
+  puts "Do you live in the UK or US?"
+  country = gets.chomp.upcase
 
+if country == "US"
   case age
     when 1..18
-      puts "Sorry, you have no permissions. "
+      puts "Sorry, no permissions. "
     when 18..21
       puts "You can vote AND smoke! Go enjoy."
     when 21..25
@@ -20,6 +20,21 @@ def check_age
       puts "Thank you. Goodbye."
       exit
     end
+
+  else
+    case age
+    when 18..@inf
+      puts "In the UK, at age 18 you may enjoy a pint."
+    when 16 || 17
+      puts "Are your parents with you? T/F?"
+      parents = gets.chomp.upcase
+      if parents == "T"
+        puts "You may order a drink."
+      else
+        puts "You have a few years before you can enjoy one with the rents."
+      end
+    end
+end
 end
 
 loop do
